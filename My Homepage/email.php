fb55770c633980b5af9119d1be9dc2302909dbce
jraupp1 <56286340+jraupp1@users.noreply.github.com>
2019-12-20 19:58:40 -0500

    Add files via upload

---
+++ My Homepage/email.php
@@ -0,0 +1,13 @@
+<? php
+
+$name = $_POST['name'];
+$request = $_POST['email'];
+$message = $_POST['message'];
+
+$to = "jeremyraupp@gmail.com";
+
+$subject = "Personal Website Contact Request";
+
+$body = "Please don't reply to this message \n\n $name"
+
+?>
